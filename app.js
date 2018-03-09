9e3d4be41e62f9adb58584baf8a55db3c5eb2da5
vls48 <vls48@drexel.edu>
2018-03-09 17:02:32 -0500

    portupdate

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ var express = require('express'); // Express web server framework
 var app = express();
 app.use(express.static(__dirname + '/public'));
 console.log('Listening on 8888');
-app.listen(8888);
+app.listen(process.env.PORT || 8888);
