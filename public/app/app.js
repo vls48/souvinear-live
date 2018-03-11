e2b93315411290c280daf5fecd278a183eb38e85
vls48 <vls48@drexel.edu>
2018-03-11 16:38:35 -0400

    commit 2 merge anisa

--- public/app/app.js
+++ public/app/app.js
@@ -5,78 +5,93 @@ app.config(['$routeProvider',
         $routeProvider.
         when('/login', {
             title: 'Login',
-            templateUrl: 'partials/login.html'
+            templateUrl: 'partials/login.html',
+            navBoolean: true
 //            controller: 'loginCtrl'
         })
             .when('/logout', {
                 title: 'Logout',
                 templateUrl: 'partials/login.html',
-                controller: 'logoutCtrl'
+                controller: 'logoutCtrl',
+                navBoolean: true
             })
             .when('/signup', {
                 title: 'Signup',
-                templateUrl: 'partials/signup.html'
+                templateUrl: 'partials/signup.html',
+                navBoolean: true
 //                controller: 'authCtrl'
             })
             .when('/main', {
                 title: 'main',
-                templateUrl: 'partials/main.html'
+                templateUrl: 'partials/main.html',
+                navBoolean: false
 //                controller: 'authCtrl'
             })
             .when('/', {
                 title: 'login',
                 templateUrl: 'partials/login.html',
+                navBoolean: true,
 //                controller: 'authCtrl',
                 role: '0'
             })
             .when('/add-entry', {
                 title: 'add entry',
-                templateUrl: 'partials/add-entry.html'
+                templateUrl: 'partials/add-entry.html',
+                navBoolean: true
 //                controller: 'authCtrl' 
             })
             .when('/hotspot', {
                 title: 'hotspot',
-                templateUrl: 'partials/hotspot.html'
+                templateUrl: 'partials/hotspot.html',
+                navBoolean: false
 //                controller: 'authCtrl' 
             })
             .when('/swapbox', {
                 title: 'swapbox',
-                templateUrl: 'partials/swapbox.html'
+                templateUrl: 'partials/swapbox.html',
+                navBoolean: false
 //                controller: 'authCtrl' 
             })            
             .when('/settings', {
                 title: 'settings',
-                templateUrl: 'partials/settings.html'
+                templateUrl: 'partials/settings.html',
+                navBoolean: false
 //                controller: 'authCtrl' 
             })
             .when('/reset', {
                 title: 'reset password',
-                templateUrl: 'partials/reset.html'
+                templateUrl: 'partials/reset.html',
+                navBoolean: true
 //                controller: 'authCtrl' 
             })
             .when('/spotify', {
                 title: 'connect to spotify',
-                templateUrl: 'partials/spotify.html'
+                templateUrl: 'partials/spotify.html',
+                navBoolean: false
 //                controller: 'authCtrl' 
             })
             .when('/pin', {
                 title: 'pin a song',
-                templateUrl: 'partials/pin.html'
+                templateUrl: 'partials/pin.html',
+                navBoolean: true
 //                controller: 'authCtrl' 
             })
             .when('/spoofy', {
                 title: 'pin ya song',
-                templateUrl: 'partials/spoofy.html'
+                templateUrl: 'partials/spoofy.html',
+                navBoolean: false
 //                controller: 'authCtrl' 
             })
             .when('/edit-entry', {
                 title: 'pin a song',
-                templateUrl: 'partials/edit-entry.html'
+                templateUrl: 'partials/edit-entry.html',
+                navBoolean: true
 //                controller: 'authCtrl' 
             })
             .when('/concert-entry', {
                 title: 'pin a song',
-                templateUrl: 'partials/concert-entry.html'
+                templateUrl: 'partials/concert-entry.html',
+                navBoolean: false
 //                controller: 'authCtrl' 
             })
             .otherwise({
@@ -100,3 +115,69 @@ app.factory('dataService', function() {
  }
 
 });
+
+app.controller('MyCtrl', function ($scope, $route, $routeParams, $location) {
+    
+  var navBoolean = $scope.navBoolean;
+  navBoolean = true;
+  $scope.$route = $route;
+  var hello = $location.path();
+  $scope.$routeParams = $routeParams;
+
+  console.log($location.path());
+  console.log(hello);
+  console.log(navBoolean);
+
+  switch (hello){
+      case '/login':
+          $scope.navBoolean = true;
+          console.log(navBoolean);
+          break;
+      case '/logout':
+          $scope.navBoolean = true;
+          console.log(navBoolean);
+          break;
+      case '/signup':
+          $scope.navBoolean = true;
+          console.log(navBoolean);
+          break;
+      case '/main':
+          $scope.navBoolean = false;
+          console.log(navBoolean);
+          break; 
+      case '/add-entry':
+          $scope.navBoolean = true;
+          console.log(navBoolean);
+          break;
+      case '/hotspot':
+          $scope.navBoolean = false;
+          console.log(navBoolean);
+          break;
+      case '/swapbox':
+          $scope.navBoolean = false;
+          console.log(navBoolean);
+          break;
+      case '/settings':
+          $scope.navBoolean = false;
+          console.log(navBoolean);
+          // $scope.$digest();
+          break;
+      case '/reset':
+          $scope.navBoolean = true;
+          console.log(navBoolean);
+          break;
+      case '/spotify':
+          $scope.navBoolean = true;
+          console.log(navBoolean);
+          // $scope.$digest();
+          break;
+      case '/pin':
+          $scope.navBoolean = false;
+          console.log(navBoolean);
+          break;
+      default:
+      $scope.navBoolean = false;
+          console.log('default' + navBoolean);
+          break;
+      } 
+  });
--- public/js/modal.js
+++ public/js/modal.js
@@ -95,10 +95,6 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
 btn2.onclick = function() {
     modal.style.display = "none";
 }
